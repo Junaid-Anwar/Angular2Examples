ad7b6e7c90df1c1e56719e9729023a1e9f8f0901
Junaid Anwar <janwar@btisinc.com>
2017-01-30 15:25:51 -0800

    Added Dynamic Custom Validation Component

---
+++ app/components/validationmessage.component.ts
@@ -0,0 +1,34 @@
+﻿import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { CustomValidators } from '../models/customvalidators';
+
+@Component({
+    moduleId: module.id.replace("dist", ""),
+    selector: 'validationmessage',
+    templateUrl: '../templates/validationmessage.html'
+})
+
+export class ValidationMessageComponent implements OnInit {
+    @Input() control: FormControl;
+    @Input() label: string;
+    @Input() message: string;
+    validationMessage: string=null
+    
+    constructor() { }
+
+    ngOnInit() {
+        this.control.valueChanges
+            .subscribe(data => this.onValueChanged(data));
+    }
+
+    onValueChanged(data?: any) {
+        this.validationMessage = null;
+
+        for (let validator in this.control.errors) {
+            if (this.control.dirty || this.control.touched) {
+                this.validationMessage = CustomValidators.getMessage(this.label, this.message, validator, this.control.errors[validator]);
+            }
+        }
+    }
+}
